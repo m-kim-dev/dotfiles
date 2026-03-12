7ec207099718a77a9c6e71f15356aa3e8d597272
Mugeon Kim <mugeon.kim.dev@gmail.com>
2026-03-13 08:43:29 +0900

    FIX: leading zero in time

--- nightlight.sh
+++ nightlight.sh
@@ -4,7 +4,7 @@
 # for exceptional case: manually change, it stays for an hour; repeat if needed
 setColorTemp() {
   export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
-  T=$(date +%H%M)  # current time
+  T=10#$(date +%H%M)  # current time
   if (( T >= 700 && T < 1800 )); then
     TEMP=3700
   elif (( T >= 1800 && T < 1900 )); then
